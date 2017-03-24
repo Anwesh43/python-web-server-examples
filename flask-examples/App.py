16afd5d34c3944861724302973dbe5e0370b16ff
Anwesh43 <anweshthecool0@gmail.com>
2017-03-24 23:40:07 +0530

    adding post example

--- flask-examples/App.py
+++ flask-examples/App.py
@@ -11,3 +11,8 @@ def helloName(name):
 def helloPerson():
     print request.args
     return "hello person whose age is {0} and name is {1}".format(request.args["age"],request.args["name"])
+@app.route('/postPerson',methods=['POST'])
+def postPerson():
+    person = request.form
+    print person
+    return "Person's name is {0} and age is {1}\n".format(person["name"],person["age"])
