2a20e78236c34b317097b9d248d30432b508ebdd
Anwesh43 <anweshthecool0@gmail.com>
2017-03-24 13:00:39 +0530

    using queries in flask app

--- flask-examples/App.py
+++ flask-examples/App.py
@@ -1,8 +1,13 @@
 from flask import *
 app = Flask(__name__)
+app.debug = True
 @app.route('/hello')
 def hello():
     return "hello world"
 @app.route('/helloName/<name>')
 def helloName(name):
     return 'hello {0}'.format(name)
+@app.route('/helloQueries')
+def helloPerson():
+    print request.args
+    return "hello person whose age is {0} and name is {1}".format(request.args["age"],request.args["name"])
