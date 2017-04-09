fb03c035982ae08ee49d381ff8d0c78043a69267
Anwesh43 <anweshthecool0@gmail.com>
2017-04-09 17:39:00 +0530

    using parameters

--- klein-framework/klein-starter-app.py
+++ klein-framework/klein-starter-app.py
@@ -3,4 +3,7 @@ from klein import route,run
 @route('/')
 def index(request):
     return "hello world"
+@route('/hello/<username>')
+def hello(request,username):
+    return "hello {0}".format(username)    
 run("localhost",8000)
